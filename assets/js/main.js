61e9a5424898c0498a959d812b4ef25c6c3d2043
shalbuz <sh.mursalov@mail.ru>
2026-03-02 18:08:05 +0300

    feat: updates styles and fix slider js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,33 @@
 ﻿$(document).on('ready', function () {
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          arrows: false
-        }
-      }
-    ]
-  });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+             {
+                breakpoint: 992, // Планшеты (меньше 992px)
+                settings: {
+                    slidesToShow: 4,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768, // Мобильные (меньше 768px)
+                settings: {
+                    slidesToShow: 3,
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
